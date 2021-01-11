923d496a439938975c2ec916c90b0e8a5c7e7606
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2021-01-11 12:11:53 -0600

    created HomeScreen with static products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-
-function App() {
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+const App = () => {
   return (
 <>
+<Header/>
+<main>
+  <Container>
+    <HomeScreen />
+  </Container>
+</main>
+<Footer/>
 </>
   );
 }
