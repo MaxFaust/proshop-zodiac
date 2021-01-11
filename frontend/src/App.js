457aedb0f9ee84e10d72ca29323461947e7a2fe6
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2021-01-11 13:41:53 -0600

    added product information to card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+
 const App = () => {
   return (
 <>
 <Header/>
-<main>
+<main className='py-3'>
   <Container>
     <HomeScreen />
   </Container>
