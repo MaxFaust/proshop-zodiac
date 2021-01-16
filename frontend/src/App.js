4cc336080c783db5449253455ade54fe5e90ab97
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2021-01-16 02:19:47 -0600

    user login reducer and action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 			<Header />
 			<main className='py-3'>
 				<Container>
-					<Route exact path='/' component={HomeScreen} />
+					<Route path='/' component={HomeScreen} exact />
 					<Route path='/product/:id' component={ProductScreen} />
 					<Route path='/cart/:id?' component={CartScreen} />
 				</Container>
