4bbc4a86f003c2205d99ed68bad34f4f6af8f410
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2021-01-19 22:52:44 -0600

    create order

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import Message from "../components/Message"
 import CheckoutSteps from "../components/CheckoutSteps"
+import { createOrder } from "../actions/orderActions"
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
+	const dispatch = useDispatch()
+
 	const cart = useSelector((state) => state.cart)
 
 	//   Calculate prices
@@ -17,8 +20,6 @@ const PlaceOrderScreen = () => {
 		cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
 	)
 	cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100)
-
-	//@Desc     Calculate tax
 	cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
 	cart.totalPrice = (
 		Number(cart.itemsPrice) +
@@ -26,8 +27,28 @@ const PlaceOrderScreen = () => {
 		Number(cart.taxPrice)
 	).toFixed(2)
 
+	const orderCreate = useSelector((state) => state.orderCreate)
+	const { order, success, error } = orderCreate
+
+	useEffect(() => {
+		if (success) {
+			history.push(`/order/${order._id}`)
+		}
+		// eslint-disable-next-line
+	}, [history, success])
+
 	const placeOrderHandler = () => {
-		console.log("order")
+		dispatch(
+			createOrder({
+				orderItems: cart.cartItems,
+				shippingAddress: cart.shippingAddress,
+				paymentMethod: cart.paymentMethod,
+				itemsPrice: cart.itemsPrice,
+				shippingPrice: cart.shippingPrice,
+				taxPrice: cart.taxPrice,
+				totalPrice: cart.totalPrice,
+			})
+		)
 	}
 
 	return (
@@ -61,7 +82,7 @@ const PlaceOrderScreen = () => {
 									{cart.cartItems.map((item, index) => (
 										<ListGroup.Item key={index}>
 											<Row>
-												<Col md={3}>
+												<Col md={1}>
 													<Image
 														src={item.image}
 														alt={item.name}
@@ -114,6 +135,9 @@ const PlaceOrderScreen = () => {
 									<Col>Total</Col>
 									<Col>${cart.totalPrice}</Col>
 								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								{error && <Message variant='danger'>{error}</Message>}
 							</ListGroup.Item>
 							<ListGroup.Item>
 								<Button
