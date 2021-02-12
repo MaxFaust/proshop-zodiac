21adab60af31528242d1ad476eb506b241bf47d6
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2021-02-12 11:03:15 -0600

    morgan and cleanup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import colors from "colors"
+import morgan from "morgan"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
@@ -10,6 +11,10 @@ import orderRoutes from "./routes/orderRoutes.js"
 import uploadRoutes from "./routes/uploadRoutes.js"
 
 const app = express()
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"))
+}
 
 app.use(express.json())
 
