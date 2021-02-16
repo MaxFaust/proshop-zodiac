031963f17df32e78cf80edd226ddda5d40674639
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2021-02-16 09:55:42 -0600

    product review front end

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -92,6 +92,7 @@ const ProductListScreen = ({ history, match }) => {
 							<th>ID</th>
 							<th>NAME</th>
 							<th>PRICE</th>
+							<th>COUNT IN STOCK</th>
 							<th>CATEGORY</th>
 							<th>BRAND</th>
 							<th></th>
@@ -103,6 +104,11 @@ const ProductListScreen = ({ history, match }) => {
 								<td>{product._id}</td>
 								<td>{product.name}</td>
 								<td>${product.price}</td>
+								<td>
+									{product.countInStock > 0
+										? `${product.countInStock}`
+										: "Out Of Stock"}
+								</td>
 								<td>{product.category}</td>
 								<td>{product.brand}</td>
 								<td>
