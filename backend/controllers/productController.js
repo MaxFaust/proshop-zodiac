e999a7f5be5761f416a83cd279796be5325f26af
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2021-02-17 11:04:12 -0600

    get top products backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -147,6 +147,15 @@ const createProductReview = asyncHandler(async (req, res) => {
 		throw new Error("Product not found")
 	}
 })
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Private
+const getTopProducts = asyncHandler(async (req, res) => {
+	const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+	res.json(products)
+})
+
 export {
 	getProducts,
 	getProductById,
@@ -154,4 +163,5 @@ export {
 	createProduct,
 	updateProduct,
 	createProductReview,
+	getTopProducts,
 }
